5d19f92899bb86b6b44e90b735815a0bbca71e4b
zhuqi259 <zhu.qi.memory@gmail.com>
2016-06-06 09:21:38 +0800

    测试删除scripts

--- src/main/scripts/deploy2LocalTomcat.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set PROJECT_DIR="D:\workspace\transform-app-server"
-set TOMCAT_HOME="D:\Software\apache-tomcat-7.0.69"
-set WAR_NAME=AppServer.war
-cd /d %PROJECT_DIR%
-call mvn clean package
-cd /d %TOMCAT_HOME%\webapps
-del /q %WAR_NAME%
-copy %PROJECT_DIR%\target\%WAR_NAME% .
